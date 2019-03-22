1a55b5cf88c9b15206df2240c6e02519aba7b35c
Nicholai Mauritzson <nicholai.mauritzson@nuclear.lu.se>
2019-03-22 09:10:34 +0100

    Quick plot, working!

--- analysis/analog/quick_plot.py
+++ analysis/analog/quick_plot.py
@@ -1,4 +1,9 @@
-# ------------------------------------------------------------------------------
+import sys
+#sys.path.insert(0, "/home/gheed/Documents/projects/TNT/code_repo") #Import my own libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 # ------------------------------------------------------------------------------
 # Quick plotting script for analog aquadaq.
 # Argument needs to be hdf5 file (pandas)
@@ -6,20 +11,58 @@
 # By Nicholai Mauritzson 
 # ------------------------------------------------------------------------------
 
-import sys
-sys.path.insert(0, "/home/gheed/Documents/projects/TNT/code_repo","r") #Import my own libraries
-from pyTagAnalysis import load_data
-import nicholai_math as nm
-import pickle
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+path="/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1811_DF.hdf5"
+df = pd.read_hdf(path) #Load data
 
-df = pd.read_hdf(path=sys.argv[0])
 
-bins=8*1024
-figure(0)
-plt.hist()
-ax.set_yscale("log")
+plt.figure(0)
+plt.hist(df.qdc_det0, 256, linewidth=2, histtype="step",label='LG, entries={}'.format(np.count_nonzero(df.qdc_det0)))
+plt.legend()
+plt.title("Long gate QDC neutron detector")
+plt.ylabel("counts")
+plt.xlabel("QDC [arb. unit]")
+plt.grid(zorder=0)
+plt.yscale("log")
 
+plt.figure(1)
+plt.hist(df.qdc_sg_det0, 256, linewidth=2, histtype="step",label='SG, entries={}'.format(np.count_nonzero(df.qdc_sg_det0)))
+plt.legend()
+plt.title("Short gate QDC neutron detector")
+plt.ylabel("counts")
+plt.xlabel("QDC [arb. unit]")
+plt.grid(zorder=0)
+plt.yscale("log")
+
+plt.figure(2)
+plt.hist(df.qdc_yap0, 256, histtype="step", linewidth=2, label='YAP_0, entries={}'.format(np.count_nonzero(df.qdc_yap0)))
+plt.hist(df.qdc_yap1, 256, histtype="step", linewidth=2, label='YAP_1, entries={}'.format(np.count_nonzero(df.qdc_yap1)))
+plt.hist(df.qdc_yap2, 256, histtype="step", linewidth=2, label='YAP_2, entries={}'.format(np.count_nonzero(df.qdc_yap2)))
+plt.hist(df.qdc_yap3, 256, histtype="step", linewidth=2, label='YAP_3, entries={}'.format(np.count_nonzero(df.qdc_yap3)))
+plt.legend()
+plt.title("QDC YAPs")
+plt.ylabel("counts")
+plt.xlabel("QDC [arb. unit]")
+plt.grid(zorder=0)
+plt.yscale("log")
+
+plt.figure(3)
+plt.hist(df.tdc_det0_yap0, 3000, histtype="step", linewidth=2, label='YAP_0, entries={}'.format(np.count_nonzero(df.tdc_det0_yap0)))
+plt.hist(df.tdc_det0_yap1, 3000, histtype="step", linewidth=2, label='YAP_1, entries={}'.format(np.count_nonzero(df.tdc_det0_yap1)))
+plt.hist(df.tdc_det0_yap2, 3000, histtype="step", linewidth=2, label='YAP_2, entries={}'.format(np.count_nonzero(df.tdc_det0_yap2)))
+plt.hist(df.tdc_det0_yap3, 3000, histtype="step", linewidth=2, label='YAP_3, entries={}'.format(np.count_nonzero(df.tdc_det0_yap3)))
+plt.legend()
+plt.title("Time of Flight")
+plt.xlabel("TOF [arb. unit]")
+plt.ylabel("counts")
+plt.grid(zorder=0)
+plt.yscale("log")
+
+
+plt.show()
+
+#plot: 
+# qdc0 LG
+# qdc0 SG
+# qdc YAP0,1,2,3 in same (step)
+# tof 0,1,2,3 in same (step)
 

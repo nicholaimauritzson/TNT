7bf1bdb97a77105d5fcf759ced8d9a7fcf0a3aa6
Nicholai Mauritzson <nicholai.mauritzson@nuclear.lu.se>
2019-04-01 11:21:42 +0200

    .

--- analysis/analog/sandbox.py
+++ analysis/analog/sandbox.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0, "/home/gheed/Documents/projects/TNT/code_repo") #Import my own libraries
 from pyTagAnalysis import load_data
 import nicholai_math as nm
+import nicholai_utility as nu
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,12 +23,12 @@ from scipy.optimize import curve_fit
 # Load and save data as HDF5 file.
 # ------------------------------------------------------------------------------
 
-# df1 = load_data("/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1838_cooked.root")
+# df1 = load_data("/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1841_cooked.root")
 # df2 = load_data("/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1816_cooked.root")
 # df3 = load_data("/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1817_cooked.root")
 # frames = [df1, df2, df3]
 # df4 = pd.concat(frames)
-# df1.to_hdf("/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1838_DF.hdf5", key="w")
+# df1.to_hdf("/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1841_DF.hdf5", key="w")
 
 # ------------------------------------------------------------------------------
 
@@ -83,74 +84,121 @@ from scipy.optimize import curve_fit
 #:::::::::::::::::::::::::::::::::::::::::::
 # Compton edge fitting
 # data = pta.load_data('/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1825_cooked.root')
-data = pd.read_hdf('/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1825_DF.hdf5')
-col = "qdc_det0"
-min = 430
-max = 550
-def compton_fit(data, col, min, max, Ef, fit_lim=None):
-    """
-    Method is designed to fit and return the edge positions and maximum electron recoil energy of a Compton distribution.
-    1) input pandas df 'data' 'col' 'min' 'max' 'Ef' 'fit_lim'
-    2) Create histogram, saves x,y data
-    3) 
-    """
-    #data is pandas df
-    #col is the name of the column containing the data
-    #Ef is the orignal foton energy
-    #fit_lim contains. Default is None. Otherwise fit_lim = [[const_min, mean_min, sigma_min],[const_max, mean_max, sigma_max]]. These are boundary constraints for the Gaussian fit functions. Put to inf or -inf to igore. 
-
-    N = np.histogram(data[col], range=[min, max], bins=(max-min)) #Create histogram and save x,y data.
-    x = N[1][:-1]   #Set bin positions as x values
-    y = N[0]        #Set bin heights as y values
-    meanTEMP = sum(x * y) / sum(y) #Calcaulate the mean value of distibution. Used as first quess for Gaussian fit.
-    sigmaTEMP = np.sqrt(sum(y * (x - meanTEMP)**2) / sum(y)) #Calculate stdev of distribution. Used as first geuss for Gaussian fit.
-
-    # const_min = 11651*0.98
-    # mean_min  = (452)
-    # sigma_min = -np.inf
-    # const_max = 11651*1.02
-    # mean_max  = np.inf#(451+10)
-    # sigma_max = np.inf
-    # fit_lim = [[const_min, mean_min, sigma_min],[const_max, mean_max, sigma_max]]
-
-    if fit_lim != None: #Check if boundary limits are enforced for Gaussian fit
-        popt, pcov = curve_fit(nm.gaussFunc, x, y, p0 = [np.max(y), meanTEMP, sigmaTEMP], bounds=fit_lim)
-    else:
-        popt, pcov = curve_fit(nm.gaussFunc, x, y, p0 = [np.max(y), meanTEMP, sigmaTEMP])
-    const, mean, sigma = popt[0], popt[1], popt[2] #saving result of fit as individual variables.
-    #Print to console: optimal fitting parameters
-    print('--- Optimal fitting parameters (scipy.curve_fit(gaussianFunc, const, mean, sigma)) ---')
-    print('const=%.4f (max height of gaussian)' % const)
-    print('mean=%.4f (mean position of gaussian)' % mean)
-    print('sigma=%.4f (standard deviation of gaussian)' % sigma)
-    print('---------------------------------------------------------------------------------------')
-    print() #Create vertical space on terminal
-    E_recoil_max = nm.comptonMax(eF) #Calculate maximum electron recoil energy
-    
-    for i in range(min,max): #Loop for finding 89% of maximum
-        if nm.gaussFunc(i, const, mean, sigma)<=0.89*const:
-            p=nm.gaussFunc(i, const, mean, sigma) #Saving compton edge value
-            print('-> Compton edge found at ADC value: %.4f' % p) #Printing compton edge value (ADC) to console
-            break
-    print('-> Maximum electron recoil energy: %.4f' % E_recoil_max)
-
-    #Test plotting
-    plt.hist(data.qdc_det0,range=[200,800], bins=600)
-    plt.plot(x,nm.gaussFunc(x, const, mean, sigma))
-    plt.show()
-
-return p, E_recoil_max
 
 
-# Fuction which fits the compton spectrum as above but will show plot for itterative testing to determine best fit.
-# Function compton fit should:
-# - plot the data and fit so that user can make changes
-# - return ADC value of compton edge
-# - print to console final Gaussian fit parameters for diagnostic.
 
-#PSD TESTING
+# BG_data = pd.read_hdf('/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1841_DF.hdf5')
+# Co60_data = pd.read_hdf('/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1831_DF.hdf5')
+# Na22_data = pd.read_hdf('/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1838_DF.hdf5')
+# Cs137_data = pd.read_hdf('/media/gheed/Seagate_Expansion_Drive1/Data/TNT/NE213_cup/Data1825_DF.hdf5')
 
-# LG = df.qdc_det0
-# SG = df.qdc_det0
-# plt.hexbin(LG, (LG-SG)/LG)
-# ply.show()
+# BG_time = 14375
+# Co60_time = 510.29 
+# Cs137_time = 3698.68
+# Na22_time = 603.77
+
+# BG_w    = np.empty(len(BG_data.qdc_det0)) #Make array same length as data set
+# Co60_w  = np.empty(len(Co60_data.qdc_det0)) #Make array same length as data set
+# Na22_w  = np.empty(len(Na22_data.qdc_det0)) #Make array same length as data set
+# Cs137_w = np.empty(len(Cs137_data.qdc_det0)) #Make array same length as data set
+
+# BG_w.fill(1/BG_time)
+# Co60_w.fill(1/Co60_time)
+# Na22_w.fill(1/Na22_time)
+# Cs137_w.fill(1/Cs137_time)
+
+# BG    = np.histogram(BG_data.qdc_det0,      bins=1024, weights=BG_w)
+# Co60  = np.histogram(Co60_data.qdc_det0,    bins=1024, weights=Co60_w)
+# Na22  = np.histogram(Na22_data.qdc_det0,    bins=1024, weights=Na22_w)
+# Cs137 = np.histogram(Cs137_data.qdc_det0,   bins=1024, weights=Cs137_w)
+
+# Co60_new = Co60[0]-BG[0]
+# Na22_new = Na22[0]-BG[0]
+# Cs137_new = Cs137[0]-BG[0]
+
+# plt.plot(Co60[1][:-1], Co60[0], label='Co60 - BG')
+# plt.plot(Co60[1][:-1], Co60_new, label='Co60 raw')
+
+# plt.plot(Na22[1][:-1], Na22[0], label='Na22 - BG')
+# plt.plot(Na22[1][:-1], Na22_new, label='Na22 raw')
+
+# plt.plot(Cs137[1][:-1], Cs137[0], label='Cs137 - BG')
+# plt.plot(Cs137[1][:-1], Cs137_new, label='Cs137 raw')
+
+# plt.legend()
+# plt.show()
+
+
+
+#_____Na-22 (0.511 keV)Boundary conditions______
+const_min = -np.inf
+mean_min  = -np.inf #...
+sigma_min = -np.inf
+const_max = np.inf
+mean_max  = 444#np.inf
+sigma_max = np.inf
+fit_lim = [[const_min, mean_min, sigma_min],[const_max, mean_max, sigma_max]]
+min = 400#441
+max = 545
+Ef = .511
+
+# #_____Cs-137 (1.3325 MeV)Boundary conditions______
+# const_min = 11651*0.98
+# mean_min  = (452)
+# sigma_min = -np.inf
+# const_max = 11651*1.02
+# mean_max  = np.inf#(451+10)
+# sigma_max = np.inf
+# fit_lim = [[const_min, mean_min, sigma_min],[const_max, mean_max, sigma_max]]
+
+# #_____Cs-137 (0.6615 MeV)Boundary condisions______
+# const_min = -np.inf
+# mean_min  = -np.inf#454#-np.inf
+# sigma_min = -np.inf
+# const_max = 3.15#np.inf
+# mean_max  = np.inf
+# sigma_max = np.inf
+# fit_lim = [[const_min, mean_min, sigma_min],[const_max, mean_max, sigma_max]]
+# min = 460
+# max = 550#535
+# Ef = .6615
+
+##_____Na-22 (1.275 keV)Boundary condisions______
+# const_min = -np.inf
+# mean_min  = -np.inf
+# sigma_min = -np.inf
+# const_max = np.inf
+# mean_max  = np.inf
+# sigma_max = np.inf
+# fit_lim = [[const_min, mean_min, sigma_min],[const_max, mean_max, sigma_max]]
+
+# col = "qdc_det0"
+# nm.comptonEdgeFit(Na22_data, col, min, max, Ef, Na22_w, BG_w, BG_data, fit_lim)
+
+
+#ERROR PROPAGATION TEST
+# print(nm.errorPropAdd((.25,25,3,100,0.010)))
+# print(nm.errorPropMulti(100, (1,2,3), (0.1,1.1,0.001)))
+# print(nm.errorPropPower(100, 25, 0.001, 3))
+# print(nm.errorPropExp(100, 0.1))
+# print(nm.errorPropGauss(12000, 400, 12500, 100, 350, 52, 100, 20))
+
+
+
+from uncertainties import ufloat
+from uncertainties.umath import * #Get all methods for library
+
+x=491.720 #My error: 5.5350
+# #MY Y values and error: 2.8118 +/- 0.0367 (Na22 @ 89%)
+uconst =ufloat(3.1593, 0.0250)
+umean = ufloat(442.1512, 2.8206)
+usigma = ufloat(102.1599, 4.5066)
+
+const = 3.1593
+const_err = .025
+mean = 442.1512
+mean_err = 2.8206
+sigma = 102.1599
+sigma_err = 4.5066
+Y = nm.gaussFunc(x, const, mean, sigma)
+print(nm.errorPropGauss(Y, x, const, const_err, mean, mean_err, sigma, sigma_err))

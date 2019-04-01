gaussianEdgeFit(): Fixed error calculation for CE at 50%

code_repo/nicholai_math.py
@@ -278,10 +278,10 @@ def comptonEdgeFit(data, col, min, max, Ef, w1=None, w2=None, BG=None, fit_lim=N
         flag1, flag2 = 0, 0
         for i in tqdm(np.arange(min, max, 0.001), desc='Caclulating errors for CE @ 50%'): #Loop for finding 50% of maximum with 3 decimal points
             val = gaussFunc(i, const, mean, sigma)
-            if val <= compare[0] and flag1 == 0:
+            if val <= compare2[0] and flag1 == 0:
                 p_err[0] = i #Saving compton edge value
                 flag1 = 1
-            if val <= compare[1] and flag2 == 0:
+            if val <= compare2[1] and flag2 == 0:
                 p_err[1] = i #Saving compton edge value
                 flag2 = 1
 